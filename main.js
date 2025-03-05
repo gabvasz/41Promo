0b39120a888ea836eac9ef859895713175817403
gabvaz <gblvaz@gmail.com>
2025-03-05 01:11:36 -0300

    adicionei BootStrap, animei com Gsap

---
+++ main.js
@@ -0,0 +1,84 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Timeline
+let tl = gsap.timeline()
+
+
+tl.from('#logo-41promo', 
+    {
+        duration: .3,
+        y: -50,
+    }
+)
+
+tl.to('.hero', {
+ opacity: 1,
+ y: -40,
+ duration: .8,
+ ease: 'power1.out'
+})
+
+tl.to("#h1", {
+    clipPath: "inset(0 0 100% 0)",
+    y: 200, // Define o quanto o texto desce
+    scrollTrigger: {
+        trigger: "#h1",
+        start: "100% center",
+        end: "bottom top",
+        scrub: true // Suaviza o movimento com o scroll
+    }
+});
+
+tl.to("#h2", {
+    clipPath: "inset(0 0 100% 0)",
+    y: 500, // Define o quanto o texto desce
+    opacity: 1,
+    scrollTrigger: {
+        trigger: "#h2",
+        start: "top center",
+        end: "bottom top",
+        scrub: true // Suaviza o movimento com o scroll
+    }
+});
+
+tl.to("#h3", {
+    clipPath: "inset(0 0 100% 0)",
+    y: 500, // Define o quanto o texto desce
+    opacity: 1,
+
+    scrollTrigger: {
+        trigger: "#h3",
+        start: "top center",
+        end: "bottom top",
+        scrub: true // Suaviza o movimento com o scroll
+    }
+});
+tl.to("#h4", {
+    clipPath: "inset(0 0 100% 0)",
+    y: 200, // Define o quanto o texto desce
+    opacity: 1,
+
+    scrollTrigger: {
+        trigger: "#h4",
+        start: "top center",
+        end: "bottom top",
+        scrub: true // Suaviza o movimento com o scroll
+    }
+});
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(500)
+  .typeString('<strong>Conexões</strong>, ')
+  .pauseFor(350)
+  .typeString('criatividade, ')
+  .pauseFor(350)
+  .typeString('celebração.')
+  .pauseFor(5000)
+  .start();
